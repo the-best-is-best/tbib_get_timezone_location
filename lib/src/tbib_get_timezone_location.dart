dd1cf4164be6797baf42edb88b16387e6a394779
Michelle <michelle.raouf@outlook.com>
2023-07-27 10:16:05 +0300

    init

---
+++ lib/src/tbib_get_timezone_location.dart
@@ -0,0 +1,78 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:tbib_get_timezone_location/gen/assets.gen.dart';
+
+/// {@template tbib_get_timezone_location}
+/// Get timezone location from time offest user
+/// {@endtemplate}
+class TbibGetTimezoneLocation {
+  /// {@macro tbib_get_timezone_location}
+  static late String dbPath;
+
+  /// init database
+  Future<void> init() async {
+    final databaseFile =
+        await _createDatabase(const $AssetsDatabaseGen().timeZone);
+    // final database = await openDatabase(databaseFile.path);
+    dbPath = databaseFile.path;
+  }
+
+  Future<File> _createDatabase(String dbName) async {
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    final dbPath = join(documentsDirectory.path, 'assets', 'database', dbName);
+
+    final exists = await databaseExists(dbPath);
+    if (!exists) {
+      // Create the directory structure if it does not exist
+      await Directory(dirname(dbPath)).create(recursive: true);
+
+      // Copy the database from the assets folder to the documents directory
+      final data =
+          await rootBundle.load('packages/tbib_get_timezone_location/$dbName');
+      final List<int> bytes = data.buffer.asUint8List(
+        data.offsetInBytes,
+        data.lengthInBytes,
+      );
+      await File(dbPath).writeAsBytes(bytes);
+    }
+
+    return File(dbPath);
+  }
+
+  Future<String?> _getUserCountryCode() async {
+    await Geolocator.requestPermission();
+    final position = await Geolocator.getCurrentPosition();
+    final placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    final countryCode = placemarks.first.country;
+    return countryCode;
+  }
+
+  /// get timezone
+  Future<void> getTimezones() async {
+    final offsetWithSeconds = DateTime.now().timeZoneOffset.inSeconds;
+    final getUserCountry = await _getUserCountryCode();
+
+    final db = await openDatabase(dbPath);
+    final countryCodeResult = await db.query(
+      'country',
+      where: 'country = ?',
+      whereArgs: [getUserCountry],
+    );
+    log('countryCodeResult: ${countryCodeResult.first['country_code']} - offset $offsetWithSeconds');
+    final result = await db.query(
+      'time_zone',
+      where: 'country_code = ? AND gmt_offset = ?',
+      whereArgs: [countryCodeResult.first['country_code'], offsetWithSeconds],
+    );
+    await db.close();
+    print(result.first['zone_name']);
+  }
+}
---
+++ lib/tbib_get_timezone_location.dart
@@ -0,0 +1,4 @@
+/// Get timezone location from time offest user
+library tbib_get_timezone_location;
+
+export 'src/tbib_get_timezone_location.dart';
