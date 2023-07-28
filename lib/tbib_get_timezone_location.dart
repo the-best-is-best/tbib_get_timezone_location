9c3f2a9f3171454c24a65be97c79b104c0b9efe1
Michelle <michelle.raouf@outlook.com>
2023-07-28 19:03:29 +0300

    change readme and use tbib_timezone_offset

--- lib/tbib_get_timezone_location.dart
+++ lib/tbib_get_timezone_location.dart
@@ -1,4 +1,6 @@
 /// Get timezone location from time offest user
 library tbib_get_timezone_location;
 
+export 'package:tbib_timezone_offset/tbib_timezone_offset.dart';
+
 export 'src/tbib_get_timezone_location.dart';
